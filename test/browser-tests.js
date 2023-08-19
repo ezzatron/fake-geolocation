23458ab89482cbc15e91c8c0967b7472bd6cc2f0
Erin Millard <contact@erin.id.au>
2023-08-19 13:19:31 +1000

    Add some browser test functions

---
+++ test/browser-tests.js
@@ -0,0 +1,142 @@
+/**
+ * This test shows that if maximumAge is 0, browsers will not return a cached
+ * position.
+ */
+async function maximumAgeAffectsCache() {
+  // cache a position
+  const positionA = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+    );
+  });
+  console.log({ positionA });
+
+  // sleep for 1ms
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1);
+  });
+
+  // get another position with maximumAge = 0
+  const positionB = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+      { maximumAge: 0 },
+    );
+  });
+  console.log({ positionB });
+
+  // check that the second position was not cached
+  if (positionB.timestamp === positionA.timestamp) {
+    console.error("❌ Position was cached");
+  } else {
+    console.log("✅ Position was not cached");
+  }
+}
+
+/**
+ * This test shows that if maximumAge is Infinity, browsers will return a cached
+ * position, even if timeout is 0.
+ */
+async function maximumAgeAffectsTimeout() {
+  // cache a position
+  const positionA = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+    );
+  });
+  console.log({ positionA });
+
+  // sleep for 1ms
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1);
+  });
+
+  // get another position with maximumAge = Infinity and timeout = 0
+  const positionB = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+      { maximumAge: Infinity, timeout: 0 },
+    );
+  });
+  console.log({ positionB });
+
+  // check that the second position was cached
+  if (positionB.timestamp === positionA.timestamp) {
+    console.log("✅ Position was cached");
+  } else {
+    console.error("❌ Position was not cached");
+  }
+}
+
+/**
+ * This test shows that the timestamp of a position is based on the time when
+ * getCurrentPosition was called, not when the position was received.
+ */
+async function timestampIsBasedOnAcquireTime() {
+  // cache a position
+  const positionA = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+    );
+  });
+  console.log({ positionA });
+
+  // sleep for 1ms
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1);
+  });
+
+  // get another position, recording timestamps
+  const acquireTime = Date.now();
+  const positionB = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p);
+      },
+      (e) => {
+        reject(e);
+      },
+      { maximumAge: 0 },
+    );
+  });
+  const receiveTime = Date.now();
+  console.log({ positionB });
+
+  // check that the timestamp is after the acquire time
+  if (positionB.timestamp > acquireTime) {
+    console.log("✅ Timestamp is after acquire time");
+  } else {
+    console.error("❌ Timestamp is before acquire time");
+  }
+
+  // log the deltas between timestamps
+  console.log({
+    acquireDelta: positionB.timestamp - acquireTime,
+    receiveDelta: positionB.timestamp - receiveTime,
+  });
+}
