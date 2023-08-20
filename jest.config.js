48731ff30469b5661ee2dcf557a1b49e2ab9db57
Erin Millard <contact@erin.id.au>
2023-08-20 15:24:21 +1000

    Switch to `@swc/jest`

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,13 @@
-// detect whether Jest has been invoked with specific test file(s)
-const isTestFile = process.argv.some((arg) => arg.endsWith(".spec.ts"));
-
 /** @type {import('jest').Config} */
 const config = {
-  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
+    "^.+\\.(t|j)sx?$": "@swc/jest",
   },
-  testMatch: [isTestFile ? "**/*.spec.ts" : "<rootDir>/test/**/*.spec.*"],
+  transformIgnorePatterns: [],
   coverageDirectory: "artifacts/coverage/jest",
   collectCoverageFrom: ["<rootDir>/src/**/*"],
 
