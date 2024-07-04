e43f3f5fea09ac260b9d72b227f517958e80af0a
Erin Millard <contact@erin.id.au>
2024-07-05 09:51:03 +1000

    Add type test for GeolocationCoordinatesParameters

---
+++ test/vitest/geolocation-coordinates-parameters.spec.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { GeolocationCoordinatesParameters } from "fake-geolocation";
+import { describe, expectTypeOf, it } from "vitest";
+
+describe("GeolocationCoordinatesParameters", () => {
+  it("accepts the same properties as GeolocationCoordinates", () => {
+    expectTypeOf<GeolocationCoordinatesParameters>().toEqualTypeOf<{
+      latitude: number;
+      longitude: number;
+      altitude: number | null;
+      accuracy: number;
+      altitudeAccuracy: number | null;
+      heading: number | null;
+      speed: number | null;
+    }>(undefined as any); // weird that this is necessary
+  });
+});
