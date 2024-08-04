71aea6564e21ad45471cdb4bb51e245295d019ba
Erin Millard <contact@erin.id.au>
2024-08-04 22:56:36 +1000

    Replace star exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "./compare-coordinates.js";
-export * from "./create-apis.js";
-export * from "./create-wrapped-apis.js";
+export { compareCoordinates } from "./compare-coordinates.js";
+export { createAPIs } from "./create-apis.js";
+export { createWrappedAPIs } from "./create-wrapped-apis.js";
 export { createDelegatedGeolocation } from "./delegated-geolocation.js";
 export type {
   IsDelegateSelected,
@@ -11,7 +11,12 @@ export {
   createCoordinates,
 } from "./geolocation-coordinates.js";
 export type { GeolocationCoordinatesParameters } from "./geolocation-coordinates.js";
-export * from "./geolocation-position-error.js";
+export {
+  GeolocationPositionError,
+  createPermissionDeniedError,
+  createPositionUnavailableError,
+  createTimeoutError,
+} from "./geolocation-position-error.js";
 export { GeolocationPosition, createPosition } from "./geolocation-position.js";
 export type { GeolocationPositionParameters } from "./geolocation-position.js";
 export { Geolocation, createGeolocation } from "./geolocation.js";
@@ -20,6 +25,7 @@ export type {
   LocationServices,
   MutableLocationServices,
 } from "./location-services.js";
-export * from "./user.js";
-export * from "./wait-for-coordinates.js";
-export * from "./wait-for-position-error.js";
+export { createUser } from "./user.js";
+export type { User } from "./user.js";
+export { waitForCoordinates } from "./wait-for-coordinates.js";
+export { waitForPositionError } from "./wait-for-position-error.js";
